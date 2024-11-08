be92ff56132440d1571f1593a9383c7afff2cf68
jeetsoni17 <jeetsoni1702@gmail.com>
2024-11-08 17:59:38 +0530

    removed public from gitignore

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,12 @@
-import React from 'react'
-
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import React from "react";
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  <div>
+    <h1>Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <Link to="/">Go back to Home</Link>
+  </div>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
